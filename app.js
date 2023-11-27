dc22cb4bb5cb4848177eda02d8749d1498449d36
AFRIN ASHAR <98964571+afrinashar@users.noreply.github.com>
2023-11-27 17:13:11 +0530

    git

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const passport = require('./config/passport');
+//const passport = require('./config/passport');
 const adminRoutes = require('./router/adminRoutes')
 const studentRouter = require('./router/studentRouter');
 const teacherRouter = require('./router/teacherRouter');
+var cors = require('cors');
+var dotenv = require('dotenv').config()
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://afrin:961215106001@cluster0.hbkqtqv.mongodb.net/students', {
+mongoose.connect( dotenv.process.env, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -17,15 +19,15 @@ mongoose.connect('mongodb+srv://afrin:961215106001@cluster0.hbkqtqv.mongodb.net/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 // Use the student router
 app.use('/admin', adminRoutes);
 app.use('/students', studentRouter);
 app.use('/teachers', teacherRouter);
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
